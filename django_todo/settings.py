e3d829c274ace6e40215bbcf0080fcd008ab8b3d
Mattias-b-Pettersson <mattep3@gmail.com>
2022-10-23 16:22:29 +0000

    settings change

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -50,8 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'django_todo.urls'
